a766236c3a984f72eeb112207e119d788af5675d
abelponce03 <abelponce03@gmail.com>
2023-07-25 19:01:08 -0400

    ++

--- script/program.sh
+++ script/program.sh
@@ -1,90 +1,101 @@
-#! /bin/bash
-# variables
-Tex_Report="informe.tex" #Nombre del archivo latex del informe
-Tex_Slides="document.tex" #Nombre del archivo latex de las presentaciones
-PDF_Report="${Tex_Report%TEX}.pdf" #Nombre del archivo pdf del informe generado
-PDF_Slides="${Tex_Slides%TEX}.pdf" #Nombre del archivo pdf de la presentacion generado
-APP_Project="Moogle-" #Nombre del proyecto de la aplicacion web
-VIEWER_CMD_DEFAULT="xdg-open" #Comando por defecto para visualizar archivos pdf
+#!/bin/bash
 
-echo "se ha ejecutado el programa"
-#Funcion para compilar un archivo latex y generar el pdf
-function compile_pdf()
+# Opción: run - Ejecutar la aplicación web
+function run_web_app() {
+    echo "Retrocediendo un directorio..."
+    cd ..
+    
+    echo "Ejecutando la aplicación web..."
+    # Comandos para iniciar tu aplicación web, por ejemplo:
+    dotnet watch run --project MoogleServer
+    # La ejecución de la aplicación web comenzará en el directorio anterior al actual.
+}
+# Variables
+TEX_REPORT="informe.tex" # Nombre del archivo LaTeX del informe
+TEX_SLIDES="document.tex" # Nombre del archivo LaTeX de las presentaciones
+PDF_REPORT="${TEX_REPORT%.tex}.pdf" # Nombre del archivo PDF del informe generado
+PDF_SLIDES="${TEX_SLIDES%.tex}.pdf" # Nombre del archivo PDF de las presentaciones generado
+APP_PROJECT="Moogle-" # Nombre del proyecto de la aplicación web ASP.NET Core
+VIEWER_CMD_DEFAULT="xdg-open" # Comando por defecto para visualizar archivos PDF
+
+# Función para compilar un archivo LaTeX y generar el PDF
+function compile_pdf() 
 {
     latexmk -pdf "$1"
 }
-#Opcion: RUN - Ejecutar el proyecto web en c#
-function run_proyect()
+
+# Función para generar el PDF del informe
+function generate_report_pdf()
 {
-  #Comandos para compilar y ejecutar el proyecto web
-  echo "Compilando y ejecutando el proyecto web"
-  cd Moogle-
-  dotnet watch run --project MoogleServer 
+    cd ..
+    cd Informe
+    echo "Compilando el informe LaTeX..."
+    compile_pdf "$TEX_REPORT"
 }
-#Opcion: REPORT - Compilar y generar el pdf del informe
-function generate__report_pdf()
-{
-    echo "Compilando el informe LaTex"
-    compile_pdf"$Tex_Report"
+
+# Función para generar el PDF de las presentaciones
+function generate_slides_pdf() 
+{   cd ..
+    cd Presentacion
+    echo "Compilando las presentaciones LaTeX..."
+    compile_pdf "$TEX_SLIDES"
 }
-#Opcion: slides - Compilar y generar el pdf de la presentacion
-function generate_slides_pdf()
+
+# Opción: show_report - Visualizar el informe PDF
+function show_report_pdf() 
 {
-    echo "Compilando la presentacion LaTex"
-    compile_pdf"$Tex_Slides"
-}
-#Opcion: show_report - Visualizar el informe PDF function show_report_pdf()
-{
-    if [ ! -f "$PDF_Report" ]; then echo "Generando el informe PDF"
-    generate__report_pdf
-    fi 
-    echo "Mostrando el informe"
-    "$VIEWER_CMD"
-    "$PDF_Report"
-}
-#Opcion: show_slides - Visualizar la presentacion PDF
-function show_slides_pdf()
-{
-    if [ ! -f "$PDF_Slides" ]; then echo "Generando la presentacion pdf"
-    generate_slides_pdf
+    cd ..
+    cd Informe
+    if [ ! -f "$PDF_REPORT" ]; then
+        echo "Generando el informe PDF..."
+        generate_report_pdf
     fi
-    echo "Mostrando la presentacion"
-    "$VIEWER_CMD"
-    "$PDF_Slides"   
+
+    echo "Mostrando el informe..."
+    "$VIEWER_CMD" "$PDF_REPORT"
 }
-#Opcion: clean - Eliminar ficheros auxiliares generados en la compilacion
-function clean_proyect () 
+
+# Opción: show_slides - Visualizar las presentaciones PDF
+function show_slides_pdf() 
 {
-    echo "Limpiando ficheros auxiliares"
-    latexmk -c
-    rm -f*.snm*.nav
-    cd "Moogle-"
-    dotnet clean
+    cd..
+    cd Presentacion
+    if [ ! -f "$PDF_SLIDES" ]; then
+        echo "Generando las presentaciones PDF..."
+        generate_slides_pdf
+    fi
+
+    echo "Mostrando las presentaciones..."
+    "$VIEWER_CMD" "$PDF_SLIDES"
 }
-#Opciones del script
-case "$1" in 
-"run")
-run_proyect
-;;
-"report")
-generate__report_pdf
-;;
-"slides")
-generate_slides_pdf
-;;
-"show_report")
-VIEWER_CMD=${2:-$VIEWER_CMD_DEFAULT}
-show_report_pdf
-;;
-"show_slides")
-VIEWER_CMD=${2:-$VIEWER_CMD_DEFAULT}
-show_slides_pdf
-;;
-"clean")
-clean_proyect
-;;
-*)
-echo "uso: $0 {run|report|slides|show_report|show_slides|clean}"
-exit 1
-;;
+
+# Opciones del script
+case "$1" in
+    "run")
+        run_web_app
+        ;;
+    "report")
+        generate_report_pdf
+        ;;
+    "slides")
+        generate_slides_pdf
+        ;;
+      "show_report")
+        VIEWER_CMD=${2:-$VIEWER_CMD_DEFAULT}
+        show_report_pdf
+        ;;
+    "show_slides")
+        VIEWER_CMD=${2:-$VIEWER_CMD_DEFAULT}
+        show_slides_pdf
+        ;;
+    "clean")
+        echo "Limpiando ficheros auxiliares..."
+        latexmk -c
+        rm -f *.snm *.nav
+        cd "$APP_PROJECT" && dotnet clean
+        ;;
+    *)
+        echo "Uso: $0 {run|report|slides|show_report|show_slides|clean}"
+        exit 1
+        ;;
 esac
